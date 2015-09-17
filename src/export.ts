53f1f488932fb807a2ae26496e666b2999baee64
Marcin Marzewski <marcin.marzewski@nopattern.net>
2015-09-17 14:34:21 +0200

    fix(src): Fixed condition, changed factory generation as factory

--- src/export.ts
+++ src/export.ts
@@ -6,8 +6,8 @@ class Export {
   static factory(name, factory) {
     if (typeof exports === 'object' && typeof module !== 'undefined') {
       module.exports[name] = factory;
-    } else if (typeof angular === 'function') {
-      angular.module('4screens.util.' + name, []).const(name, factory);
+    } else if (typeof angular === 'object') {
+      angular.module('4screens.util.' + name, []).factory(name, function() { return factory; });
     } else {
       if (!window['4screens']) {
         window['4screens'] = {};
